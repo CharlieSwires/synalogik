0ed21a8cdd4fec7ce18dc52cf05a335b07dc01c2
CharlieSwires <charlieswires@gmail.com>
2021-05-19 19:50:39 +0100

    First

---
+++ src/main/java/ReadFile.java
@@ -0,0 +1,127 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFile {
+    static ReadFile rf = null;
+    static final int  MAX_LENGTH_OF_WORD = 100;
+    static final String ls = "\n";
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Usage java ReadFile <filename>");
+            System.exit(0);
+        }
+        //Standard read file into a string with EOL characters
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+        // delete the last new line separator
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        reader.close();
+
+        String content = stringBuilder.toString();
+        System.out.println(results(preprocess(content)));
+    }
+    protected static String preprocess (String input) {
+        //Join up words using a -EOL
+        input = input.replaceAll("-"+ls, "");
+        //replace EOL with <space>
+        input = input.replaceAll(ls, " ");
+        //replace "
+        input = input.replaceAll("\"", "");
+        //replace ,.()
+        input = input.replaceAll("[,.)(]", "");
+        System.out.println("Input="+input);
+        return input;
+    }
+
+    class ListOfWords {
+        private ArrayList<String> listOfWords;
+
+        public ArrayList<String> getListOfWords() {
+            return listOfWords;
+        }
+
+        public void setListOfWords(ArrayList<String> listOfWords) {
+            this.listOfWords = listOfWords;
+        }
+    }
+    
+    class Index {
+        public int index;
+    }
+    public static String results(String content) {
+        rf = new ReadFile();
+        StringBuilder sb = new StringBuilder();
+        //lists of words
+        ListOfWords[] listOfWordsGroupedBySize = new ListOfWords[MAX_LENGTH_OF_WORD];
+        for(int i = 0; i < MAX_LENGTH_OF_WORD; i++) {
+            ListOfWords listOfWords = rf.new ListOfWords();
+            listOfWords.setListOfWords(new ArrayList<String>());
+            listOfWordsGroupedBySize[i] = listOfWords;
+        }
+        String word = null;
+        Index index = rf.new Index();
+        index.index = 0;
+        Integer wordCount = 0;
+        Long sum = 0l;
+        while((word = readWord(content,index))!=null) {
+            wordCount++;
+            if(word.length() < MAX_LENGTH_OF_WORD) {
+                listOfWordsGroupedBySize[word.length()].getListOfWords().add(word);
+                sum += word.length();
+            } else {
+                System.out.println("MAX_LENGTH_OF_WORD exceeded");
+            }
+        }
+        sb.append("Word count="+wordCount+ls);
+        
+        Double average = sum / (1.0*wordCount);
+        sb.append("Average word length="+average+ls);
+        int max = 0;
+        for(int i = 1; i<MAX_LENGTH_OF_WORD;i++) {
+            if(listOfWordsGroupedBySize[i].getListOfWords().size()> 0) {
+                sb.append("Number of words of length "+i+" is "+listOfWordsGroupedBySize[i].getListOfWords().size()+ls);
+                if (listOfWordsGroupedBySize[i].getListOfWords().size() > max) {
+                    max = listOfWordsGroupedBySize[i].getListOfWords().size();
+                }
+            }
+        }
+        List<Integer> matches = new ArrayList<Integer>();
+        sb.append("The most frequently occuring word length is "+max+", for word lengths of ");
+        int matchCount = 0;
+        for(int i = 1; i<MAX_LENGTH_OF_WORD;i++) {
+            if(listOfWordsGroupedBySize[i].getListOfWords().size() == max) {
+                matches.add(i);
+                matchCount++;
+                if (matchCount == 1 ) {
+                    sb.append(i);
+                }else {
+                    sb.append(" & "+ i);
+                }
+            }
+        }
+        sb.append(ls);
+        return sb.toString();
+    }
+
+    private static String readWord(String content, Index index) {
+        StringBuilder returnString = new StringBuilder();
+        for(int i = index.index; i < content.length();) {
+            if (content.charAt(i) == ' ' || (i >= content.length()-1)) {
+                index.index = i+1;
+                return returnString.toString();
+            }
+            returnString.append(content.charAt(i++));
+        }
+        index.index = content.length();
+        return null;
+    }
+
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Tests {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        String content = "This is a \"test\" string (which is a bit com-\nplex).";
+        System.out.println(ReadFile.results(ReadFile.preprocess(content)));
+        Assert.assertEquals("Word count=10\n"+
+                "Average word length=3.4\n"+
+                "Number of words of length 1 is 2\n"+
+                "Number of words of length 2 is 2\n"+
+                "Number of words of length 3 is 1\n"+
+                "Number of words of length 4 is 2\n"+
+                "Number of words of length 5 is 1\n"+
+                "Number of words of length 6 is 2\n"+
+                "The most frequently occuring word length is 2, for word lengths of 1 & 2 & 4 & 6\n",ReadFile.results(ReadFile.preprocess(content)));
+    }
+    @Test
+    public void test2() {
+        String content = "Hello world & good morning. The date is 18/05/2016";
+        System.out.println(ReadFile.results(ReadFile.preprocess(content)));
+        Assert.assertEquals("Word count=9\n"+
+                "Average word length=4.444444444444445\n"+
+                "Number of words of length 1 is 1\n"+
+                "Number of words of length 2 is 1\n"+
+                "Number of words of length 3 is 1\n"+
+                "Number of words of length 4 is 2\n"+
+                "Number of words of length 5 is 2\n"+
+                "Number of words of length 7 is 1\n"+
+                "Number of words of length 9 is 1\n"+
+                "The most frequently occuring word length is 2, for word lengths of 4 & 5\n",ReadFile.results(ReadFile.preprocess(content)));
+    }
+
+}
